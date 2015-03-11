11fadc89e992334cfd9d51a2c09a165f6078565d
Jilson Asis <j.asis@klab.com>
2015-03-11 14:41:26 +0800

    Created View page for Thread links, Update controller,model, and view for thread

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -7,5 +7,12 @@ class ThreadController extends AppController
         $threads = Thread::getAll();           
         
         $this->set(get_defined_vars());
-    }                        
+    }
+
+    public function view()
+    {
+        $thread = Thread::get(Param::get('thread_id'));
+
+        $this->set(get_defined_vars());
+    }
 }
--- app/models/thread.php
+++ app/models/thread.php
@@ -3,16 +3,30 @@
 class Thread extends AppModel                    
 {
     public static function getAll()                
-   {
+    {
         $threads = array();
                     
         $db = DB::conn();
+
         $rows = $db->rows('SELECT * FROM thread');
         
         foreach ($rows as $row) {                    
-           $threads[] = new Thread($row);
+            $threads[] = new Thread($row);
         }
-                    
-       return $threads;
-    }                        
+                
+        return $threads;
+    }
+
+    public static function get($id)            
+    {
+        $db = DB::conn();
+ 
+        $row = $db->row('SELECT * FROM thread WHERE id = ?', array($id));
+
+        if(!$row) {
+            throw new RecordNotFoundException('no record found');
+        }
+
+        return new self($row);                    
+    }
 }
